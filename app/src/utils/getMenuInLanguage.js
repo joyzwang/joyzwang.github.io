feb55635a6a23dfbb7d466df40d260518281387a
SY <shelley.yang4596@gmail.com>
2026-02-09 23:17:08 +0000

    cleanup: getMenuInLanguage

--- app/src/utils/getMenuInLanguage.js
+++ app/src/utils/getMenuInLanguage.js
@@ -9,7 +9,7 @@ export function getMenuInLanguage(language) {
   var output = [];
   for (let i = 0; i < menu.length; i++) {
     const thisMenuItem = menu[i];
-    const item = new menuItem(thisMenuItem[`${language}`], thisMenuItem.link);
+    const item = new menuItem(thisMenuItem[language], thisMenuItem.link);
     output.push(item);
   }
   return output;
