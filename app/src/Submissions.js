3205d41962db4936ad057ef139e6049db07255c3
SY <shelley.yang4596@gmail.com>
2026-02-09 23:43:12 +0000

    add: working submissions

--- app/src/Submissions.js
+++ app/src/Submissions.js
@@ -1,17 +1,22 @@
-import { submissionContent } from "./content.js";
+import { getPageInLanguage } from "./utils/getPageInLanguage";
 import { useOutletContext } from "react-router-dom";
 
 function Submissions({ language }) {
-  const content = submissionContent[useOutletContext(language)];
-
-  return (
-    <>
-      <h2> {content.heading1} </h2>
-      <p>{content.p1} </p>
-      <h2> {content.heading2} </h2>
-      <p>{content.p2} </p>
-    </>
-  );
+  const content = getPageInLanguage('submissions',useOutletContext(language));
+  
+    for (const key in content) {
+      if ( content[key] === undefined) {
+        content[key] = '';
+      }
+    }
+  
+    return (
+      <>
+        <div className="highlight" dangerouslySetInnerHTML={{__html: content.highlight}}></div>
+        <h2 dangerouslySetInnerHTML={{__html: content.title}} />
+        <div className="body" dangerouslySetInnerHTML={{__html: content.body}}></div>
+      </>
+    );
 }
 
 export default Submissions;
