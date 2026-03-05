1f08876c14664ca9b3bf44408c973634b4348b90
SY <shelley.yang4596@gmail.com>
2026-03-05 15:55:33 +0000

    add: title

--- app/src/index.js
+++ app/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <title>Between the Lines</title>
     <App />
   </React.StrictMode>,
 );
