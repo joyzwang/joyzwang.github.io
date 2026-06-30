63d10c1abe4ca2751ee35df72e5d92c8b01272f1
floralfish <shelley.yang4596@gmail.com>
2026-07-01 01:33:51 +0300

    attempt: env

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,19 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page from "./Page";
 import Layout from "./Layout";
 import "./style.css";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route exact index element={<Page name="home" />} />
-          <Route exact path="about" element={<Page name="about" />} />
+          <Route exact path="#/about" element={<Page name="about" />} />
           <Route exact path="read" element={<Page name="read" pdf="true" />} />
-          <Route path="404" element={<h3>Not found</h3>} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
