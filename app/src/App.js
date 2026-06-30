f9211390da0ee485e8bad9cfdceefdc11d347db6
floralfish <shelley.yang4596@gmail.com>
2026-07-01 01:19:06 +0300

    revert to browserrouter

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page from "./Page";
 import Layout from "./Layout";
 import "./style.css";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Page name="home" />} />
@@ -13,7 +13,7 @@ function App() {
           <Route path="read" element={<Page name="read" pdf="true" />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
