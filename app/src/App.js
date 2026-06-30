63963302242ddaa5a5e2973c6a6f264c0b32926f
floralfish <shelley.yang4596@gmail.com>
2026-07-01 01:17:33 +0300

    attempt: hashrouter + redirects

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Page from "./Page";
 import Layout from "./Layout";
 import "./style.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Page name="home" />} />
@@ -13,7 +13,7 @@ function App() {
           <Route path="read" element={<Page name="read" pdf="true" />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
