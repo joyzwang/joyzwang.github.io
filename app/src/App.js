2d8c76423cbd1fdd18a53c00b3adb4b60603d603
floralfish <shelley.yang4596@gmail.com>
2026-07-01 01:24:19 +0300

    attempt: hashrouter with 404

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Page from "./Page";
 import Layout from "./Layout";
 import "./style.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Page name="home" />} />
-          <Route path="about" element={<Page name="about" />} />
-          <Route path="read" element={<Page name="read" pdf="true" />} />
+          <Route exact index element={<Page name="home" />} />
+          <Route exact path="about" element={<Page name="about" />} />
+          <Route exact path="read" element={<Page name="read" pdf="true" />} />
+          <Route path="404" element={<h3>Not found</h3>} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
